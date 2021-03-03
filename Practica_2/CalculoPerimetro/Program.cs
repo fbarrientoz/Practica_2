3e96aaff7273486b28c9ecd6674a4af972cea82b
fbarrientos <fbarrientos@uabc.edu.mx>
2021-03-02 21:03:41 -0600

    Calculo Perimetro

---
+++ Practica_2/CalculoPerimetro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoPerimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definir Variables 
+            int a= 10;
+            int b= 10;
+            int c= 7;
+            int p = a + b + c;
+            Console.WriteLine("El perimetro del trangulo es: " + p);
+            Console.WriteLine("Presiona cualquier tecla para continuar");
+            Console.ReadKey();
+
+        }
+    }
+}
