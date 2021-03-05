66b22ca0ca6b1ef15e01b5c28b8e7e5e0e53de4b
fbarrientos <fbarrientos@uabc.edu.mx>
2021-03-04 20:27:34 -0600

    Programa que escribe edad y nombre y se hizo una conversion

---
+++ Practica_2/Ejercicio_StringInt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Nombre;
+            String LineaEdad;
+            int edad;
+            Console.WriteLine("Escribe tu nombre: ");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Escribe la edad: ");
+            LineaEdad = Console.ReadLine();
+            edad = int.Parse(LineaEdad);
+            Console.WriteLine("Tu nombre es: " + Nombre + " y tienes " + edad + "años");
+            Console.WriteLine("Presiona cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
